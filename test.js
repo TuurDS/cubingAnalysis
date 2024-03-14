90e097dff77baf15a8cac2efe1104d131475ced9
Tuur De Schepper <tuur.deschepper@student.hogent.be>
2024-03-14 16:03:34 +0100

    fix inserted session averages

---
+++ test.js
@@ -0,0 +1,81 @@
+const fs = require("fs-extra");
+//read the file
+const f = fs.readFileSync("C:/Users/tuurd/projects/cubingAnalysis/src/Data/cross-sessionsExports/2024_01_05_size58158.json", "utf8"); 
+const json = JSON.parse(f);
+
+//read the file text.json
+const f2 = fs.readFileSync("text.json", "utf8");
+const json2 = JSON.parse(f2);
+
+const caculateAverage = (start, end) => {
+//caculate the average time for each object in this file that is between the start and end date
+//the objects in this json file look like this:
+
+//[
+//...
+// {
+//     "time": 10.29,
+//     "scramble": "L' B D2 B' U' R' F' U' R2 L2 D2 B' D2 B2 U2 B U2 L2 B' U L'",
+//     "date": 1704380789
+// },
+//...
+//]
+    let times = [];
+    //loop over each object in the json file
+    for (const { time, date } of json) {
+        //parse the date to an integer
+        const d = Number(date);
+        //check if the date is between the start and end date
+        if (d >= start && d <= end) {
+            //put the time in an array
+            times.push(time);
+            }
+        }
+
+    //remove the best and worst time
+    times = times.sort((a, b) => a - b).slice(1, -1);
+    //calculate the average
+    return Math.round(times.reduce((a, b) => a + b * 1000, 0) / times.length);
+}
+
+//{
+//...
+// "476": {
+//       "name": "1.4 333",
+//       "opt": {},
+//       "rank": 476,
+//       "stat": [
+//         100,
+//         0,
+//         9340
+//       ],
+//       "date": [
+//         1704378622,
+//         1704381170
+//       ]
+//     }
+//...
+//}
+
+//this is the format of the json file
+//loop over each key in the json object
+for (const key in json2) {
+    const { name, opt, rank, stat, date } = json2[key];
+
+    if (null != stat[2]) {  
+        continue;
+    }
+
+    //get the start and end date and parse them to an integer
+    const [start, end] = date.map(Number);
+    const avr = caculateAverage(start, end);
+    //generate the same object with the average time added and the start and end date parsed to a number
+    json2[key] = { name, opt, rank, stat: [stat[0],stat[1],avr], date: [start, end] };
+}
+// write to textnew.json
+fs.writeFileSync("textnew.json", JSON.stringify(json2, null, 2));
+
+//done part 1
+
+//iterate over all the files in cross-sessionsExports
+const files = fs.readdirSync("C:/Users/tuurd/projects/cubingAnalysis/src/Data/cross-sessionsExports");
