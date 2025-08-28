b9e38f9fee3ba94e8b5084aa7b5144717c8765ed
TuurDS <tuur.deschepper@outlook.com>
2025-08-28 17:03:18 +0200

    started transfering to DotNet

---
+++ CubingAnalysis/CubingAnalysis.Core/services/SessionState.cs
@@ -0,0 +1,20 @@
+﻿namespace CubingAnalysis.Core.Services;
+
+public class SessionState
+{
+    private List<object> _value = [];
+
+    // Event triggered when the value changes
+    public event Action? OnChange;
+
+    // Getter/Setter
+    public List<object> Value
+    {
+        get => _value;
+        set
+        {
+            _value = value ?? []; // in case someone tries to set null
+            OnChange?.Invoke();
+        }
+    }
+}
